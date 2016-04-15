b39e78817b900e8b7f418d14485821c54f578219
bxia33 <bxia33@gatech.edu>
2016-04-15 09:06:10 -0400

    add php db requirement header for each php

--- handyman/ToolAvailability.php
+++ handyman/ToolAvailability.php
@@ -1,8 +1,30 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+error_reporting(E_ALL ^ E_DEPRECATED);
+
+/* connect to database */	
+
+$connect = mysql_connect("localhost", "root", "");
+
+if (!$connect) {
+
+	die("Failed to connect to database");
+
+}
+
+mysql_select_db("handyman") or die( "Unable to select database");
+
+$errorMsg = "";
+$emptyMsg = "";
+
+session_start();
+
+if (!isset($_SESSION['email'])) {
+
+	header('Location: login.php');
+
+	exit();
+
+}
+?>
--- handyman/reservationSum.php
+++ handyman/reservationSum.php
@@ -5,4 +5,31 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+error_reporting(E_ALL ^ E_DEPRECATED);
 
+/* connect to database */	
+
+$connect = mysql_connect("localhost", "root", "");
+
+if (!$connect) {
+
+	die("Failed to connect to database");
+
+}
+
+mysql_select_db("handyman") or die( "Unable to select database");
+
+
+$errorMsg = "";
+
+session_start();
+
+if (!isset($_SESSION['email'])) {
+
+	header('Location: login.php');
+
+	exit();
+
+}
+
+?>

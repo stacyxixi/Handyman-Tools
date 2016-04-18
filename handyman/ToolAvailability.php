4158b6cb92be98b3e8aa4dcf4bd7db65635fc089
bxia33 <bxia33@gatech.edu>
2016-04-18 03:19:27 -0400

    fix bugs in tool availability

--- handyman/ToolAvailability.php
+++ handyman/ToolAvailability.php
@@ -36,7 +36,7 @@ $end_date = $_SESSION['enddate'];
 //$start_date = date("Y-m-d 00:00:00", $start_date);
 //$end_date = date("Y-m-d 00:00:00", $end_date);
 
-$query = "SELECT Tool_ID, Abbrdesc, Deposit, Rental FROM tool WHERE Is_sold = 'N' and Tool_Type = '$Tool_Type' and Tool_ID not in (select tool.Tool_ID from tool, reserve, reservation where ((reservation.start_date< '$start_date' and reservation.end_date> '$start_date') or(reservation.start_date< '$end_date' and reservation.end_date> '$end_date')) and reserve.reservation_number = reservation.reservation_number and tool.Tool_ID = reserve.Tool_ID) and tool_id not in (select tool_id from service_order where (start_date< '$start_date' and end_date> '$start_date') or (start_date< '$end_date' and end_date> '$end_date'))";
+$query = "SELECT Tool_ID, Abbrdesc, Deposit, Rental FROM tool WHERE Is_sold = 'N' and Tool_Type = '$Tool_Type' and Tool_ID not in (select tool.Tool_ID from tool, reserve, reservation where ((reservation.start_date<= '$start_date' and reservation.end_date>= '$start_date') or(reservation.start_date<= '$end_date' and reservation.end_date>= '$end_date')) and reserve.reservation_number = reservation.reservation_number and tool.Tool_ID = reserve.Tool_ID) and tool_id not in (select tool_id from service_order where (start_date<= '$start_date' and end_date>= '$start_date') or (start_date<= '$end_date' and end_date>= '$end_date'))";
 
 $resultx = mysql_query($query);
 
--- handyman/process.php
+++ handyman/process.php
@@ -13,7 +13,7 @@ $tools = array();
 if (isset($_SESSION['start_date']) && isset($_SESSION['end_date'])) {
     $start_date = $_SESSION['start_date'];
     $end_date = $_SESSION['end_date'];
-    $query = "SELECT distinct Tool_ID, Abbrdesc FROM tool WHERE Is_sold = 'N' and Tool_Type = '$Tool_Type' and Tool_ID not in (select tool.Tool_ID from tool, reserve, reservation where ((reservation.start_date< '$start_date' and reservation.end_date> '$start_date') or (reservation.start_date< '$end_date' and reservation.end_date> '$end_date')) and reserve.reservation_number = reservation.reservation_number and tool.Tool_ID = reserve.Tool_ID and tool.tool_id not in (select tool_id from service_order where (start_date< '$start_date' and end_date> '$start_date') or (start_date< '$end_date' and end_date> '$end_date')))";
+    $query = "SELECT distinct Tool_ID, Abbrdesc FROM tool WHERE Is_sold = 'N' and Tool_Type = '$Tool_Type' and Tool_ID not in (select tool.Tool_ID from tool, reserve, reservation where ((reservation.start_date<= '$start_date' and reservation.end_date>= '$start_date') or (reservation.start_date<= '$end_date' and reservation.end_date>= '$end_date')) and reserve.reservation_number = reservation.reservation_number and tool.Tool_ID = reserve.Tool_ID and tool.tool_id not in (select tool_id from service_order where (start_date<= '$start_date' and end_date>= '$start_date') or (start_date<= '$end_date' and end_date>= '$end_date')))";
     $result = mysqli_query($dbc,$query);
     while ($row = mysqli_fetch_array($result)){
         $data = $row['Tool_ID'].":".$row['Abbrdesc'];
